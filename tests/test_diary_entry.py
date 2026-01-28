587188fca97f9a703d6482bd95654df691f7013d
Barry <barryrichards05@gmail.com>
2026-01-28 14:19:18 +0000

    extra test for no mobile numbers found

--- tests/test_diary_entry.py
+++ tests/test_diary_entry.py
@@ -15,3 +15,7 @@ def test_extracts_single_mobile_number_complex():
 def test_extracts_multiple_mobile_numbers():
     entry_1 = DiaryEntry('Extract this number: 07123456789; and this one: 07987654321; and this one: 07981254764')
     assert entry_1.extract_mobile_numbers() == ['07123456789', '07987654321', '07981254764']
+
+def test_no_mobile_number_found():
+    entry_1 = DiaryEntry('No mobile numbers here...')
+    assert entry_1.extract_mobile_numbers() == []
